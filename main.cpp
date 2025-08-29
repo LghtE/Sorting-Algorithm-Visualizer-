f4acaf981ab2f13616fed5c711605b8779c8d135
Ehsen Fazili <62301479+LghtE@users.noreply.github.com>
2025-08-29 22:30:10 +0530

    SDL added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
-using namespace std;
+#include "inc/SDL.h"
+
+#include <random>
+#include <ranges>
+#include <algorithm>
+
+#include <vector>
+
+#undef main
+
 
 int main()
 {
-    cout << "Hello world!";
+    
+    std::random_device rd;
+    std::uniform_int_distribution d(1,99);
+
+    // Populating vector with random numbers .. //
+
+    std::vector<int> v;
+
+    for (int i = 0; i < 200; i++)
+    {
+        v.push_back(d(rd));
+    }
+
+
+    // Sorting Algorithm (Selection Sort)// 
+
+    for (unsigned int i = 0; i < v.size(); i++)
+    {
+        for (unsigned int j = i; j < v.size(); j++)
+        {
+            if (v[j] < v[i])
+            {
+                std::swap(v[j], v[i]);
+            }
+        }
+    }
+
+    for (int i: v)
+    {
+        std::cout << i << " ";
+    }
+
     return 0;
 }
